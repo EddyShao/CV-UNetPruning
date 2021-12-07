2066b03facf4968a3fc0c191c888b75f2e5e82cd
Muyang Xu <74348157+Shirley-Cullen@users.noreply.github.com>
2021-12-06 19:48:47 -0500

    upload dataloader for GTA5 Dataset

---
+++ CVFramework/data/GTA5/dataloader.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms as T
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import cv2
+#
+# GTA5 Dataset:
+# mean = [0.28689554, 0.32513303, 0.28389177]
+# std = [0.18696375, 0.19017339, 0.18720214]
+class GTA5(Dataset):
+
+    def __init__(self, img_path, mask_path, X, mean, std, transform=None):
+        self.img_path = img_path
+        self.mask_path = mask_path
+        self.X = X
+        self.transform = transform
+        self.mean = mean
+        self.std = std
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, i):
+        img = cv2.imread(self.img_path + self.X[i] + '.jpg')
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.mask_path + '0' + self.X[i] + '.png', cv2.IMREAD_GRAYSCALE)
+
+        # how to transform?
+        if self.transform:
+            augmented = self.transform(image=img, mask=mask)
+            img = Image.fromarray(augmented['image'])
+            mask = augmented['mask']
+        else:
+            img = Image.fromarray(img)
+        trans = T.Compose(
+            [T.ToTensor(),
+             T.Normalize(self.mean, self.std)]
+        )
+        img = trans(img)
+        # simply calling the below will not work, you have to forward it to return something,
+        # thus, we have to use T.Compose and then call it.
+        # img = T.ToTensor()
+        # img = T.Normalize(self.mean, self.std)
+        mask = torch.from_numpy(mask).long()
+
+        # if self.patches:
+        #     img, mask = self.tiles(img, mask)
+
+        return img, mask
+
+
+class GTA5_test(Dataset):
+
+    def __init__(self, img_path, mask_path, X):
+        self.img_path = img_path
+        self.mask_path = mask_path
+        self.X = X
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        img = cv2.imread(self.img_path + self.X[idx] + '.jpg')
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.mask_path + '0' + self.X[idx] + '.png', cv2.IMREAD_GRAYSCALE)
+
+        mask = torch.from_numpy(mask).long()
+
+        return img, mask
