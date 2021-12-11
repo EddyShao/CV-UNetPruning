c6be4250d3f4ea0b1641d8ed40118043274d612d
EddyShao <eddyszh@outlook.com>
2021-12-11 11:57:21 -0500

    Update train.sh

--- CVFramework/train.sh
+++ CVFramework/train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=pnv
+#SBATCH --job-name=UNET
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=32GB
+#SBATCH --mem=128GB
 #SBATCH --time=8:00:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=zs1542@nyu.edu # put your email here if you want emails
@@ -10,16 +10,17 @@
 #SBATCH --error=NominalV_%j.err
 #SBATCH --gres=gpu:1 # How much gpu need, n is the number
 
-echo "Your NetID is: $1"
-echo "Your environment is: $2"
+echo "Your NetID is: zs1542"
+echo "Your environment is: py38"
 
 module purge
 module load anaconda3/2020.07
 
 echo "start training"
-source activate /scratch/$1/envs/$2
+source activate /scratch/zs1542/envs/py38/
+
 
 cd /scratch/zs1542/CV-FinalProject/CVFramework
-python main.py
+python /scratch/zs1542/CV-FinalProject/CVFramework/train_unet.py
 
 echo "FINISH"
